c99f64db6ac2adfdeb86d34ce88726ce43934bea
manish <manishkeer530@gmail.com>
2025-10-30 17:07:29 +0530

    backend url changed

--- src/service/axiosInstance.ts
+++ src/service/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
-// const API_BASE_URL = "https://nine-geosurvey-hub-backend.onrender.com";
+// const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://nine-geosurvey-hub-backend.onrender.com";
 
 
 // Create an Axios instance
